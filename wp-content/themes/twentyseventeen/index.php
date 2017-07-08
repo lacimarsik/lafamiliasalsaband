0411d2ebb9ead96a1258c3f6ac2b499f7ba0df52
lacimarsik <laci.marsik@gmail.com>
2017-07-08 22:33:01 +0200

    Formatting for posts

--- wp-content/themes/twentyseventeen/index.php
+++ wp-content/themes/twentyseventeen/index.php
@@ -61,7 +61,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
+	<?php //get_sidebar(); ?>
 </div><!-- .wrap -->
 
 <?php get_footer();
--- wp-content/themes/twentyseventeen/single.php
+++ wp-content/themes/twentyseventeen/single.php
@@ -23,9 +23,9 @@ get_header(); ?>
 					get_template_part( 'template-parts/post/content', get_post_format() );
 
 					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+					//if ( comments_open() || get_comments_number() ) :
+						//comments_template();
+					//endif;
 
 					the_post_navigation( array(
 						'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
@@ -37,7 +37,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
+	<?php //get_sidebar(); ?>
 </div><!-- .wrap -->
 
 <?php get_footer();
